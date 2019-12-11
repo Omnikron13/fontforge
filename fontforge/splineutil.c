Omnikron13/fontforge

commit 7ee5300b21c7e76571340138b3263e4b83a27d55

Clean up drag outline rendering
* Don't draw splines that aren't selected
* Don't copy the whole SPL when checking for selected splines

fontforge/splineutil.c
@@ -1446,6 +1446,42 @@ static SplinePointList *SplinePointListCopySpiroSelected1(SplinePointList *spl) 
 return( head );
 }
 
+bool SplinePointListCheckSelected1(const SplinePointList *base, bool spiro, bool *allsel) {
+    bool anysel = false;
+    if (allsel) {
+        *allsel = true;
+    }
+    if (spiro) {
+        for (int i = 0; i < base->spiro_cnt; ++i) {
+            if (SPIRO_SELECTED(&base->spiros[i])) {
+                anysel = true;
+                if (!allsel) {
+                    return anysel;
+                }
+            } else if (allsel) {
+                *allsel = false;
+            }
+        }
+    } else {
+        SplinePoint *first = NULL, *pt;
+        for (pt = base->first; pt != NULL && pt != first; pt = pt->next->to) {
+            if (pt->selected) {
+                anysel = true;
+                if (!allsel) {
+                    return anysel;
+                }
+            } else if (allsel) {
+                *allsel = false;
+            }
+            if (first == NULL)
+                first = pt;
+            if (pt->next == NULL)
+                break;
+        }
+    }
+    return anysel;
+}
+
 SplinePointList *SplinePointListCopy(const SplinePointList *base) {
     SplinePointList *head=NULL, *last=NULL, *cur;
 
@@ -1463,18 +1499,10 @@ return( head );
 SplinePointList *SplinePointListCopySelected(SplinePointList *base) {
     SplinePointList *head=NULL, *last=NULL, *cur=NULL;
     SplinePoint *pt, *first;
-    int anysel, allsel;
+    bool anysel, allsel;
 
     for ( ; base!=NULL; base = base->next ) {
-	anysel = false; allsel = true;
-	first = NULL;
-	for ( pt=base->first; pt!=NULL && pt!=first; pt = pt->next->to ) {
-	    if ( pt->selected ) anysel = true;
-	    else allsel = false;
-	    if ( first==NULL ) first = pt;
-	    if ( pt->next==NULL )
-	break;
-	}
+	anysel = SplinePointListCheckSelected1(base, false, &allsel);
 	if ( allsel )
 	    cur = SplinePointListCopy1(base);
 	else if ( anysel )
@@ -1492,17 +1520,11 @@ return( head );
 
 SplinePointList *SplinePointListCopySpiroSelected(SplinePointList *base) {
     SplinePointList *head=NULL, *last=NULL, *cur=NULL;
-    int anysel, allsel;
+    bool anysel, allsel;
     int i;
 
     for ( ; base!=NULL; base = base->next ) {
-	anysel = false; allsel = true;
-	for ( i=0; i<base->spiro_cnt-1; ++i ) {
-	    if ( SPIRO_SELECTED(&base->spiros[i]) )
-		anysel = true;
-	    else
-		allsel = false;
-	}
+	anysel = SplinePointListCheckSelected1(base, true, &allsel);
 	if ( allsel )
 	    cur = SplinePointListCopy1(base);
 	else if ( anysel )
@@ -1630,29 +1652,11 @@ return( last );
 SplinePointList *SplinePointListRemoveSelected(SplineChar *sc,SplinePointList *base) {
     SplinePointList *head=NULL, *last=NULL, *next;
     SplinePoint *pt, *first;
-    int anysel, allsel;
+    bool anysel, allsel;
 
     for ( ; base!=NULL; base = next ) {
 	next = base->next;
-	anysel = false; allsel = true;
-	if ( !sc->inspiro || !hasspiro()) {
-	    first = NULL;
-	    for ( pt=base->first; pt!=NULL && pt!=first; pt = pt->next->to ) {
-		if ( pt->selected ) anysel = true;
-		else allsel = false;
-		if ( first==NULL ) first = pt;
-		if ( pt->next==NULL )
-	    break;
-	    }
-	} else {
-	    int i;
-	    for ( i=0; i<base->spiro_cnt; ++i ) {
-		if ( SPIRO_SELECTED(&base->spiros[i]) )
-		    anysel = true;
-		else
-		    allsel = false;
-	    }
-	}
+	anysel = SplinePointListCheckSelected1(base, sc->inspiro && hasspiro(), &allsel);
 	if ( allsel ) {
 	    SplinePointListMDFree(sc,base);
     continue;
@@ -1976,7 +1980,7 @@ SplinePointList *SplinePointListTransform( SplinePointList *base, real transform
 
 SplinePointList *SplinePointListSpiroTransform(SplinePointList *base, real transform[6], int allpoints ) {
     SplinePointList *spl;
-    int allsel, anysel;
+    bool allsel, anysel;
     int i;
 
 
@@ -1984,12 +1988,7 @@ SplinePointList *SplinePointListSpiroTransform(SplinePointList *base, real trans
 return( SplinePointListTransform(base,transform,tpt_AllPoints));
 
     for ( spl = base; spl!=NULL; spl = spl->next ) {
-	allsel = true; anysel=false;
-	for ( i=0; i<spl->spiro_cnt-1; ++i )
-	    if ( spl->spiros[i].ty & 0x80 )
-		anysel = true;
-	    else
-		allsel = false;
+	anysel = SplinePointListCheckSelected1(spl, true, &allsel);
 	if ( !anysel )
     continue;
 	if ( allsel ) {

fontforge/splineutil.h
@@ -81,6 +81,7 @@ extern SplineChar *SFSplineCharCreate(SplineFont *sf);
 extern SplineFont *SplineFontFromPSFont(FontDict *fd);
 extern SplinePointList *SPLCopyTransformedHintMasks(RefChar *r, SplineChar *basesc, BasePoint *trans,int layer);
 extern SplinePointList *SPLCopyTranslatedHintMasks(SplinePointList *base, SplineChar *basesc, SplineChar *subsc, BasePoint *trans);
+extern bool SplinePointListCheckSelected1(const SplinePointList *base, bool spiro, bool *allsel);
 extern SplinePointList *SplinePointListCopy1(const SplinePointList *spl);
 extern SplinePointList *SplinePointListCopySelected(SplinePointList *base);
 extern SplinePointList *SplinePointListCopySpiroSelected(SplinePointList *base);

fontforgeexe/charview.c
@@ -211,7 +211,6 @@ Color cvbutton3dedgedarkcol = 0x707070;
 
 // Format is 0x AA RR GG BB.
 
-static void isAnyControlPointSelectedVisitor(SplinePoint* splfirst, Spline* s, SplinePoint* sp, void* udata );
 static int CV_OnCharSelectorTextChanged( GGadget *g, GEvent *e );
 static void CVHScrollSetPos( CharView *cv, int newpos );
 
@@ -2817,24 +2816,6 @@ static int CVExposeGlyphFill(CharView *cv, GWindow pixmap, GEvent *event, DRect*
     return(filled);
 }
 
-struct CVExpose_PreTransformSPL_ud
-{
-    int dopoints;
-    CharView *cv;
-    GWindow pixmap;
-    Color fg;
-    DRect* clip;
-    enum outlinesfm_flags strokeFillMode;
-};
-
-
-static void CVExpose_PreTransformSPL_fe( SplinePointList *spl, struct CVExpose_PreTransformSPL_ud* d )
-{
-    CVDrawSplineSetSpecialized( d->cv, d->pixmap, spl,
-				d->fg, d->dopoints, d->clip, d->strokeFillMode,
-				DraggingComparisonAlphaChannelOverride );
-}
-
 static void CVExposeReferences( CharView *cv, GWindow pixmap, SplineChar* sc, int layer, DRect* clip )
 {
     RefChar *rf = 0;
@@ -3007,16 +2988,10 @@ static void CVExpose(CharView *cv, GWindow pixmap, GEvent *event ) {
     /*
      * If we have a pretransform_spl and the user wants to see it then show it to them
      */
-     if( cv->p.pretransform_spl )
-     {
-	 struct CVExpose_PreTransformSPL_ud d;
-	 d.dopoints = 1;
-	 d.cv = cv;
-	 d.pixmap = pixmap;
-	 d.fg = DraggingComparisonOutlineColor;
-	 d.clip = &clip;
-	 d.strokeFillMode = sfm_stroke_trans;
-	 g_list_foreach( cv->p.pretransform_spl, (GFunc)CVExpose_PreTransformSPL_fe, &d );
+     if(cv->p.pretransform_spl) {
+         CVDrawSplineSetSpecialized(cv, pixmap, cv->p.pretransform_spl,
+            DraggingComparisonOutlineColor, 1, &clip, sfm_stroke_trans,
+            DraggingComparisonAlphaChannelOverride);
      }
 
     /* The call to CVExposeGlyphFill() above will have rendered a filled glyph already. */
@@ -4619,44 +4594,35 @@ typedef struct lastselectedpoint
     spiro_cp *lastselcp;
 } lastSelectedPoint;
 
-
-void CVFreePreTransformSPL( CharView* cv )
-{
-    if( cv->p.pretransform_spl )
-    {
-	g_list_foreach( cv->p.pretransform_spl, (GFunc)SplinePointListFree, NULL );
-	g_list_free( cv->p.pretransform_spl );
-    }
-    cv->p.pretransform_spl = 0;
-}
-
 static void CVMaybeCreateDraggingComparisonOutline( CharView* cv )
 {
-    if( !prefs_create_dragging_comparison_outline )
-	return;
-    if( !cv )
-	return;
-    if( cv->p.pretransform_spl )
-	CVFreePreTransformSPL( cv );
-
-    Layer* l = cv->b.layerheads[cv->b.drawmode];
-    if( !l || !l->splines )
-	return;
-
-    SplinePointList* spl = l->splines;
-    for( ; spl; spl = spl->next )
-    {
-	int anySel = 0;
-	SPLFirstVisitPoints( spl->first, isAnyControlPointSelectedVisitor, &anySel );
-	if( anySel || (cv->b.sc->inspiro && hasspiro()))
-	{
-	    cv->p.pretransform_spl = g_list_append( cv->p.pretransform_spl,
-						    SplinePointListCopy(spl) );
-	}
+    if (!prefs_create_dragging_comparison_outline)
+        return;
+    if (!cv)
+        return;
+    if (cv->p.pretransform_spl) {
+        SplinePointListFree(cv->p.pretransform_spl);
+        cv->p.pretransform_spl = NULL;
     }
 
-}
+    Layer* l = cv->b.layerheads[cv->b.drawmode];
+    if (!l || !l->splines)
+        return;
 
+    if (cv->p.anysel) {
+        SplinePointList *cur = NULL;
+        for (SplinePointList *spl = l->splines; spl; spl = spl->next) {
+            if (SplinePointListCheckSelected1(spl, cv->b.sc->inspiro && hasspiro(), NULL)) {
+                if (cur == NULL) {
+                    cv->p.pretransform_spl = cur = SplinePointListCopy1(spl);
+                } else {
+                    cur->next = SplinePointListCopy1(spl);
+                    cur = cur->next;
+                }
+            }
+        }
+    }
+}
 
 static void CVSwitchActiveSC( CharView *cv, SplineChar* sc, int idx )
 {
@@ -5548,7 +5514,8 @@ static void CVMouseUp(CharView *cv, GEvent *event ) {
 	cv->pressed = NULL;
     }
     cv->p.pressed = false;
-    CVFreePreTransformSPL( cv );
+    SplinePointListFree(cv->p.pretransform_spl);
+    cv->p.pretransform_spl = NULL;
     update_spacebar_hand_tool(cv);
 
     if ( cv->p.rubberbanding ) {
@@ -7338,7 +7305,8 @@ static void CVMenuDraggingComparisonOutline(GWindow gw, struct gmenuitem *mi, GE
     CharView *cv = (CharView *) GDrawGetUserData(gw);
 
     int checked = mi->ti.checked;
-    CVFreePreTransformSPL( cv );
+    SplinePointListFree(cv->p.pretransform_spl);
+    cv->p.pretransform_spl = NULL;
     prefs_create_dragging_comparison_outline = checked;
     SavePrefs(true);
 }
@@ -7653,13 +7621,6 @@ static void getAllControlPointsVisitor(SplinePoint* splfirst, Spline* s, SplineP
     g_hash_table_insert( ret, sp, 0 );
     g_hash_table_insert( ret, sp, 0 );
 }
-static void isAnyControlPointSelectedVisitor(SplinePoint* splfirst, Spline* s, SplinePoint* sp, void* udata )
-{
-    int* ret = (int*)udata;
-    if( sp->selected )
-	(*ret) = 1;
-}
-
 
 /**
  * Get a hash table with all the selected BCP in it.
@@ -12562,7 +12523,7 @@ static void _CharViewCreate(CharView *cv, SplineChar *sc, FontView *fv,int enc,i
     cv->b.fv = &fv->b;
     cv->map_of_enc = fv->b.map;
     cv->enc = enc;
-    cv->p.pretransform_spl = 0;
+    cv->p.pretransform_spl = NULL;
     cv->b.drawmode = dm_fore;
 
     memset(cv->showback,-1,sizeof(cv->showback));

fontforgeexe/cvpointer.c
@@ -50,20 +50,7 @@ int CVAnySel(CharView *cv, int *anyp, int *anyr, int *anyi, int *anya) {
     int i;
 
     for ( spl = cv->b.layerheads[cv->b.drawmode]->splines; spl!=NULL && !anypoints; spl = spl->next ) {
-	if ( cv->b.sc->inspiro && hasspiro()) {
-	    for ( i=0; i<spl->spiro_cnt-1; ++i )
-		if ( SPIRO_SELECTED(&spl->spiros[i])) {
-		    anypoints = true;
-	    break;
-		}
-	} else {
-	    first = NULL;
-	    if ( spl->first->selected ) anypoints = true;
-	    for ( spline=spl->first->next; spline!=NULL && spline!=first && !anypoints; spline = spline->to->next ) {
-		if ( spline->to->selected ) anypoints = true;
-		if ( first == NULL ) first = spline;
-	    }
-	}
+        anypoints = SplinePointListCheckSelected1(spl, cv->b.sc->inspiro && hasspiro(), NULL);
     }
     for ( rf=cv->b.layerheads[cv->b.drawmode]->refs; rf!=NULL && !anyrefs; rf=rf->next )
 	if ( rf->selected ) anyrefs = true;
@@ -150,7 +137,8 @@ int CVClearSel(CharView *cv) {
     int needsupdate = 0;
     AnchorPoint *ap;
 
-    CVFreePreTransformSPL( cv );
+    SplinePointListFree(cv->p.pretransform_spl);
+    cv->p.pretransform_spl = NULL;
     
     cv->lastselpt = NULL; cv->lastselcp = NULL;
     for ( spl = cv->b.layerheads[cv->b.drawmode]->splines; spl!=NULL; spl = spl->next )

fontforge/baseviews.h
@@ -76,7 +76,7 @@ typedef struct pressedOn {
     int spiro_index;		/* index of a clicked spiro_cp, or */
 			/* if they clicked on the spline between spiros, */
 			/* this is the spiro indexof the preceding spiro */
-    GList_Glib*      pretransform_spl; /* If we want to draw an image of the original spl while doing something
+    SplinePointList *pretransform_spl; /* If we want to draw an image of the original spl while doing something
 					* this is a copy of that original spl */
 } PressedOn;
 

fontforge/views.h
@@ -1475,8 +1475,6 @@ extern void visitAllControlPoints( GHashTable *col, visitSelectedControlPointsVi
 extern void CVVisitAdjacentToSelectedControlPoints( CharView *cv, bool preserveState,
 						    visitSelectedControlPointsVisitor f, void* udata );
 
-extern void CVFreePreTransformSPL( CharView* cv );
-
 extern bool CVShouldInterpolateCPsOnMotion( CharView* cv );
 
 extern int CVNearRBearingLine( CharView* cv, real x, real fudge );

commit 24e76dd6ae9a812a23a03cbd2154459c0c340d9c

Fix Spiro hndling in SplinePointListCheckSelected1 (#4057)
* Fix Spiro handling in SplinePointListCheckSelected1

This closes #4051. @jtanx threw away a distinction that existed in the code before; sometimes spiros[spiro_cnt] was iterated through, sometimes only spiros[spiro_cnt-1]. This restores that distinction, which turns out to be necessary for proper Spiro handling.

#4051 was a regression caused by #3898 and affected all commits after 0c07d38 inclusive.

fontforge/splineutil.c
@@ -1451,13 +1451,13 @@ static SplinePointList *SplinePointListCopySpiroSelected1(SplinePointList *spl) 
 return( head );
 }
 
-bool SplinePointListCheckSelected1(const SplinePointList *base, bool spiro, bool *allsel) {
+bool SplinePointListCheckSelected1(const SplinePointList *base, bool spiro, bool *allsel, bool skip_spiro_end) {
     bool anysel = false;
     if (allsel) {
         *allsel = true;
     }
     if (spiro) {
-        for (int i = 0; i < base->spiro_cnt; ++i) {
+        for (int i = 0; i < base->spiro_cnt-(skip_spiro_end?1:0); ++i) {
             if (SPIRO_SELECTED(&base->spiros[i])) {
                 anysel = true;
                 if (!allsel) {
@@ -1507,7 +1507,7 @@ SplinePointList *SplinePointListCopySelected(SplinePointList *base) {
     bool anysel, allsel;
 
     for ( ; base!=NULL; base = base->next ) {
-	anysel = SplinePointListCheckSelected1(base, false, &allsel);
+	anysel = SplinePointListCheckSelected1(base, false, &allsel, false);
 	if ( allsel )
 	    cur = SplinePointListCopy1(base);
 	else if ( anysel )
@@ -1529,7 +1529,7 @@ SplinePointList *SplinePointListCopySpiroSelected(SplinePointList *base) {
     int i;
 
     for ( ; base!=NULL; base = base->next ) {
-	anysel = SplinePointListCheckSelected1(base, true, &allsel);
+	anysel = SplinePointListCheckSelected1(base, true, &allsel, true);
 	if ( allsel )
 	    cur = SplinePointListCopy1(base);
 	else if ( anysel )
@@ -1661,7 +1661,7 @@ SplinePointList *SplinePointListRemoveSelected(SplineChar *sc,SplinePointList *b
 
     for ( ; base!=NULL; base = next ) {
 	next = base->next;
-	anysel = SplinePointListCheckSelected1(base, sc->inspiro && hasspiro(), &allsel);
+	anysel = SplinePointListCheckSelected1(base, sc->inspiro && hasspiro(), &allsel, false);
 	if ( allsel ) {
 	    SplinePointListMDFree(sc,base);
     continue;
@@ -2002,7 +2002,7 @@ SplinePointList *SplinePointListSpiroTransform(SplinePointList *base, real trans
 return( SplinePointListTransform(base,transform,tpt_AllPoints));
 
     for ( spl = base; spl!=NULL; spl = spl->next ) {
-	anysel = SplinePointListCheckSelected1(spl, true, &allsel);
+	anysel = SplinePointListCheckSelected1(spl, true, &allsel, true);
 	if ( !anysel )
     continue;
 	if ( allsel ) {

fontforge/splineutil.h
@@ -81,7 +81,7 @@ extern SplineChar *SFSplineCharCreate(SplineFont *sf);
 extern SplineFont *SplineFontFromPSFont(FontDict *fd);
 extern SplinePointList *SPLCopyTransformedHintMasks(RefChar *r, SplineChar *basesc, BasePoint *trans,int layer);
 extern SplinePointList *SPLCopyTranslatedHintMasks(SplinePointList *base, SplineChar *basesc, SplineChar *subsc, BasePoint *trans);
-extern bool SplinePointListCheckSelected1(const SplinePointList *base, bool spiro, bool *allsel);
+extern bool SplinePointListCheckSelected1(const SplinePointList *base, bool spiro, bool *allsel, bool skip_spiro_end);
 extern SplinePointList *SplinePointListCopy1(const SplinePointList *spl);
 extern SplinePointList *SplinePointListCopySelected(SplinePointList *base);
 extern SplinePointList *SplinePointListCopySpiroSelected(SplinePointList *base);

fontforgeexe/charview.c
@@ -4587,7 +4587,7 @@ static void CVMaybeCreateDraggingComparisonOutline( CharView* cv )
     if (cv->p.anysel) {
         SplinePointList *cur = NULL;
         for (SplinePointList *spl = l->splines; spl; spl = spl->next) {
-            if (SplinePointListCheckSelected1(spl, cv->b.sc->inspiro && hasspiro(), NULL)) {
+            if (SplinePointListCheckSelected1(spl, cv->b.sc->inspiro && hasspiro(), NULL, false)) {
                 if (cur == NULL) {
                     cv->p.pretransform_spl = cur = SplinePointListCopy1(spl);
                 } else {

fontforgeexe/cvpointer.c
@@ -51,7 +51,7 @@ int CVAnySel(CharView *cv, int *anyp, int *anyr, int *anyi, int *anya) {
     int i;
 
     for ( spl = cv->b.layerheads[cv->b.drawmode]->splines; spl!=NULL && !anypoints; spl = spl->next ) {
-        anypoints = SplinePointListCheckSelected1(spl, cv->b.sc->inspiro && hasspiro(), NULL);
+        anypoints = SplinePointListCheckSelected1(spl, cv->b.sc->inspiro && hasspiro(), NULL, true);
     }
     for ( rf=cv->b.layerheads[cv->b.drawmode]->refs; rf!=NULL && !anyrefs; rf=rf->next )
 	if ( rf->selected ) anyrefs = true;